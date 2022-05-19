be329f98e816624b24f02749764c1badd204e95a
Maria Enache <mariadenache@gmail.com>
2022-05-19 16:29:47 +0300

    first

---
+++ homeworkPractice1.py
@@ -0,0 +1,47 @@
+firstName = input("Enter your firstname ")
+
+lastName = input("Enter your lastname")
+
+intrest = input("Enter your interest")
+
+print(f'your Firstname is {firstName}')
+print(f'your Lastname is {lastName}')
+print(f'your intrest is {intrest}')
+
+
+#********************************************
+
+num1 = int(input("Enter your first number"))
+
+num2 = int(input("Enter your second number"))
+
+num3 = int(input("Enter your third number"))
+
+sum = (num1 + num3 + num2) * 3 
+print(f"The value of yor numbers {(num1 + num3 + num2) * 3 }")
+
+
+#*********************************************
+
+widthPOOl = int(input("Enter value"))
+
+lenghtPOOL = int(input("Enter value"))
+
+hightPOOL = int(input("Enter value"))
+
+
+sum = widthPOOl * lenghtPOOL * hightPOOL
+print(f"Pool's cubic meter value is {widthPOOl * lenghtPOOL * hightPOOL}")
+
+
+
+#***************************************
+
+weekly_work_hours = int(input("Enter number of hours worked in a week"))
+hourly_pay = int(input("Enter amount earned per hour"))
+
+sum = weekly_work_hours * hourly_pay 
+print(f"Your total pay is {weekly_work_hours * hourly_pay}")
+
+
+
